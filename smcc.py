135f0b6ddf79236fa5a83e70a9552a0d78fb8144
Александр Ничипорчук <alexnychyporchuk@gmail.com>
2022-09-06 13:43:19 +0200

    CoordStoring with GUI

--- smcc.py
+++ smcc.py
@@ -1,16 +1,28 @@
+from cgitb import text
 from logging import root
+from threading import Timer
+
 import tkinter as tk
-from tkinter import Button, Label, Place, filedialog, Text
+from tkinter import Button, Frame, Label, Place, Toplevel, filedialog, Text
 
 
 from pynput.mouse import Listener
-
+counter=0
 def on_click(x,y,button,pressed):
-   if pressed:
-      print("Mouse clicked at ({0},{1} with {2})".format(x,y,button))
-
-#with Listener(on_click=on_click) as listener:
-#   listener.join()
+      if pressed:
+         print("Mouse clicked at ({0},{1} with {2})".format(x,y,button))
+def clicker():
+   with Listener(on_click=on_click) as listener:
+      Timer(5, listener.stop).start()
+      global counter
+      counter=counter+1
+      global step
+      if counter==1:
+         step.config(text="Отметьте точку для отправки поста")
+      if counter==2:
+         step.config(text="Отметьте точку для отправки поста\n при максимальном размере\n поля ввода")
+      print(counter)
+      listener.join()
 
 root=tk.Tk()
 root.configure(background="#263D42")
@@ -22,7 +34,11 @@ print(screen_width)
 print(screen_height)
 root.geometry(f"200x200+0+{screen_height-300}")
 root.resizable(False,False)
-storeCoo = tk.Button(master=root, text="Open File", padx=25,
-                     pady=10, fg='#263D42', bg="red", command=on_click)
+root.wm_attributes("-topmost",True)
+
+storeCoo = tk.Button(master=root, text="Отметить", padx=25,
+                     pady=10, fg='#263D42', bg="red", command=clicker)
+step=Label(master=root,text="Отметьте точку для создания поста")
+step.pack()
 storeCoo.pack()
 root.mainloop()
