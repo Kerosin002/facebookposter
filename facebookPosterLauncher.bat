5135f65ea3e96df1ec713a33b33f3751ecd0f852
Александр Ничипорчук <alexnychyporchuk@gmail.com>
2022-09-07 11:37:23 +0200

    Updating GitIgnore

--- facebookPosterLauncher.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo PROGRAM IS NOW RUNNING
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""D:\Job\facebookposter\facebookPosterLauncher.ps1""' -Verb RunAs}"
-ps1'"
--- mouseMvTest.py
+++
@@ -1,9 +0,0 @@
-import os
-import subprocess
-import pyautogui
-import time
-import webbrowser
-time.sleep(1)
-pyautogui.moveTo(680,231)
-#pyautogui.moveTo(540,225)
-#pyautogui.moveTo(680,1070)
